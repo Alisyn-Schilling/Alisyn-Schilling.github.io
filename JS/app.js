237338bbffb819ff483468f02b261902ed81657a
alisyn-schilling <aeschilling.87@gmail.com>
2020-07-07 21:17:08 -0400

    trying out a different hiding technique. have it partially working. need to get transition working

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,5 @@
 const homeButton = document.getElementById("home-button");
+const homeName = document.getElementById("name");
 
 const aboutMeButton = document.getElementById("about-me-button");
 const aboutMeSection = document.getElementById("about-me-content");
@@ -9,32 +10,44 @@ const projectsSection = document.getElementById("projects");
 const projectsContainer = document.getElementById("projects-container");
 
 aboutMeButton.addEventListener("click", () => {
-  projectsSection.style.transform = "scale(0)";
-  projectsContainer.style.transform = "scale(0)";
-  if (aboutMeSection.style.transform === "scale(1)") {
-    aboutMeSection.style.transform = "scale(0)";
-    aboutMeContainer.style.transform = "scale(0)";
-  } else {
-    aboutMeSection.style.transform = "scale(1)";
-    aboutMeContainer.style.transform = "scale(1)";
-  }
+  homeName.style.display = "none";
+  aboutMeContainer.style.display = "block";
+  projectsContainer.style.display = "none";
+  //   homeName.style.opacity = "0";
+  //   projectsSection.style.transform = "scale(0)";
+  //   projectsContainer.style.transform = "scale(0)";
+  //   if (aboutMeSection.style.transform === "scale(1)") {
+  //     aboutMeSection.style.transform = "scale(0)";
+  //     aboutMeContainer.style.transform = "scale(0)";
+  //   } else {
+  //     aboutMeSection.style.transform = "scale(1)";
+  //     aboutMeContainer.style.transform = "scale(1)";
+  //   }
 });
 
 homeButton.addEventListener("click", () => {
-  aboutMeSection.style.transform = "scale(0)";
-  aboutMeContainer.style.transform = "scale(0)";
-  projectsSection.style.transform = "scale(0)";
-  projectsContainer.style.transform = "scale(0)";
+  homeName.style.display = "block";
+  aboutMeContainer.style.display = "none";
+  aboutMeContainer.style.display = "none";
+  //   homeName.style.opacity = "100";
+  //   aboutMeSection.style.transform = "scale(0)";
+  //   aboutMeContainer.style.transform = "scale(0)";
+  //   projectsSection.style.transform = "scale(0)";
+  //   projectsContainer.style.transform = "scale(0)";
 });
 
 projectsButton.addEventListener("click", () => {
-  aboutMeSection.style.transform = "scale(0)";
-  aboutMeContainer.style.transform = "scale(0)";
-  if (projectsSection.style.transform === "scale(1)") {
-    projectsSection.style.transform = "scale(0)";
-    projectsContainer.style.transform = "scale(0)";
-  } else {
-    projectsSection.style.transform = "scale(1)";
-    projectsContainer.style.transform = "scale(1)";
-  }
+  homeName.style.display = "none";
+  projectsContainer.style.display = "flex";
+  aboutMeContainer.style.display = "none";
+  //   homeName.style.opacity = "0";
+  //   aboutMeSection.style.transform = "scale(0)";
+  //   aboutMeContainer.style.transform = "scale(0)";
+  //   if (projectsSection.style.transform === "scale(1)") {
+  //     projectsSection.style.transform = "scale(0)";
+  //     projectsContainer.style.transform = "scale(0)";
+  //   } else {
+  //     projectsSection.style.transform = "scale(1)";
+  //     projectsContainer.style.transform = "scale(1)";
+  //   }
 });
